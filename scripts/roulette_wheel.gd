57ae5924f5cbc4bae49b8c9db941a0224ddb2a97
Ted Landauer <tedlandauer@gmail.com>
2026-01-31 15:56:03 -0800

    playing around with timers and sprite rotation

--- scripts/roulette_wheel.gd
+++ scripts/roulette_wheel.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
 
+#var betTimer = get_tree().create_timer(30).timeout
 
 
+
+var spinning = false
+var spinStop
+var gameEnd = false
 
 
 
@@ -12,8 +17,50 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("I'm a wheel")
+	
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == KEY_SPACE:
+			print("Space was pressed and spinning is, " + str(spinning))
+			spinning = true
+			print("now spinning is, " + str(spinning))
+			
+			await get_tree().create_timer(10).timeout
+			spinning = false
+			print("Timeout is here and spinning is, " + str(spinning))
+			
+			#$Timer.start()
+			#print($Timer.time_left())
+
+func spinWheel():
+	while (spinning == true):
+		print("I'm spinning!")
+		rotate(0.05)
+	
+			
+			
+			
+			
+#func _on_timer_timeout():
+	#timer.timeout.connect(_on_timer_timeout())
+	
+
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	spinWheel()
+	
+	#while (gameEnd == false):
+		#spinStart = randi_range(0, 5)
+		#spinStop = randi_range(0, 5)
+		#if spinStart == spinStop:
+			#gameEnd == true
+		#position.x += 1 * delta
+		#position.y += 1 * delta
+		#rotate(0.05)
+		
+		#print("this is spinStart and spinStop " + str(spinStart) + ", " + str(spinStop))
