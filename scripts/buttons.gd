c7ae18bb5f3b387d6e354f5ef7537352a9cd0ba2
Ted Landauer <tedlandauer@gmail.com>
2026-02-08 16:49:48 -0800

    made spawned chips appear on top of buttons and started working on button clicking/betting area rendering only functionality

---
+++ scripts/buttons.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_st_12_pressed() -> void:
+	print("bet placed")
+	#if $game.chipColor == 0:
+		#print("blue chip bet placed")
+	
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,14 @@
 extends Node2D
-
-var blueChip: = preload("res://scenes/blue_chip.tscn")
 #Should have called this Main
 
+var blueChip: = preload("res://scenes/blue_chip.tscn")
+var purpleChip: = preload("res://scenes/purple_chip.tscn")
+var yellowChip: = preload("res://scenes/yellow_chip.tscn")
+
+#variable to determine which chip is needed, 0: blue, 1: purple, 2: yellow
+var chipColor = 0
+
+var inBettingArea = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,38 +18,79 @@ func spawnBlueChip():
 	var newBlueChip = blueChip.instantiate()
 	var mouse: Vector2 = get_viewport().get_mouse_position()
 	
-	#newBlueChip.scale = Vector2(1, 1)
+	newBlueChip.top_level = true
 	newBlueChip.scale.x = .1
 	newBlueChip.scale.y = .1
-	#newBlueChip.position.x = (mouse.x - 250) / 2.5
-	#newBlueChip.position.y = (mouse.y - 205) / 2.5
 	newBlueChip.position = mouse
-	#newBlueChip.position.y = mouse.y
+	
 	add_child(newBlueChip)
 	print("added blue chip")
-	print("Mouse Position is..." + str(mouse))
-	print("Chip Position without setting is..." + str(newBlueChip.position))
-	print("Viewport window size is..." + str(get_viewport().size.x) + ", " + str(get_viewport().size.y))
-	#print("Camera Position is..." + str($Camera2D.position))
+	
+func spawnPurpleChip():
+	var newPurpleChip = purpleChip.instantiate()
+	var mouse: Vector2 = get_viewport().get_mouse_position()
+	
+	newPurpleChip.top_level = true
+	newPurpleChip.scale.x = .1
+	newPurpleChip.scale.y = .1
+	newPurpleChip.position = mouse
+	
+	add_child(newPurpleChip)
+	print("added purple chip")
+	
+func spawnYellowChip():
+	var newYellowChip = yellowChip.instantiate()
+	var mouse: Vector2 = get_viewport().get_mouse_position()
+	
+	newYellowChip.top_level = true
+	newYellowChip.scale.x = .1
+	newYellowChip.scale.y = .1
+	newYellowChip.position = mouse
+	
+	add_child(newYellowChip)
+	print("added yellow chip")
+
+func checkBettingArea():
+	if $BettingArea.mouse_entered():
+		inBettingArea = true
+		print("in the betting area")
+	elif $BettingArea.mouse_exited():
+		inBettingArea = false
+		print("out of betting area")
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			#print("Space was pressed and spinning is, " + str(spinning))
-			## WIP
-			#$"ChipsOld/Blue Chip".instance()
-			#blueChip.instance()
+		if event.button_index == MOUSE_BUTTON_LEFT and inBettingArea == true:
 			print ("Mouse Clicked")
-			spawnBlueChip()
-			
-			#print("now spinning is, " + str(spinning))
 			
 			
 			
-			
-			#print("Timeout is here and spinning is, " + str(spinning))
-
+			match chipColor:
+				0:
+					spawnBlueChip()
+				1:
+					spawnPurpleChip()
+				2:
+					spawnYellowChip()
+				_:
+					print("Something went wrong")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+
+func _on_purple_chip_pressed() -> void:
+	print("Purple Chip Selected")
+	chipColor = 1
+	
+
+func _on_blue_chip_pressed() -> void:
+	print("Blue Chip Selected")
+	chipColor = 0
+	
+
+func _on_yellow_chip_pressed() -> void:
+	print("Yellow Chip Selected")
+	chipColor = 2
+	
